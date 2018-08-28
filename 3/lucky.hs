728d6ca81080a0b7e1d78b46899fb7b296a20583
kamojiro <tamagoma002@yahoo.co.jp>
2018-08-28 13:02:37 +0900

    first

---
+++ 3/lucky.hs
@@ -0,0 +1,23 @@
+lucky :: Int -> String
+lucky 7 = "LUCKY NUMBER SEVEN!"
+lucky x = "Sorry, you're out of luck, pal!"
+
+sayMe :: Int -> String
+sayMe 1 = "One!"
+sayMe 2 = "Two!"
+sayMe 3 = "Three!"
+sayMe 4 = "Four!"
+sayMe 5 = "Five!"
+sayMe x = "Not between 1 and 5"
+
+factorial :: Integer -> Integer
+factorial 0 = 1
+factorial n = n*factorial(n-1)
+
+addVectors :: (Double, Double) -> (Double, Double) -> (Double, Double)
+--addVectors a b = (fst a + fst b, snd a + snd b)
+addVectors (x1, y1) (x2, y2) = (x1 + y1, x2 + y2)
+
+head' :: [a] -> a
+head' [] = error "Can't call head on an empty list, dummy"
+head' (x:_) = x
