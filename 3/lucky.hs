d8cd491be3362f7e126fcebf8613a6c4d3cdd37d
kamojiro <tamagoma002@yahoo.co.jp>
2018-09-07 00:03:04 +0900

    hgoehgoe

--- 3/lucky.hs
+++ 3/lucky.hs
@@ -21,3 +21,27 @@ addVectors (x1, y1) (x2, y2) = (x1 + y1, x2 + y2)
 head' :: [a] -> a
 head' [] = error "Can't call head on an empty list, dummy"
 head' (x:_) = x
+
+tell :: (Show a) => [a] -> String
+tell [] = "The line is empty"
+tell (x:[]) = "The list has one element: " ++ show x
+tell (x:y:[]) = "The list has two elements: " ++ show x ++ " and " ++ show y
+tell (x:y:_) = "This list is long. The first two elements are: " ++ show x ++ " and " ++ show y
+
+firstLetter :: String -> String
+firstLetter "" = "Empty string, whoops"
+firstLetter all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
+
+--bmiTell :: Double -> String
+--bmiTell bmi
+--  | bmi <= 18.5 = "You're underweight, you emo, you!"
+--  | bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"
+--  | bmi <= 30.0 = "You're fat! Lose some weight, fatty!"
+--  | otherwise   = "Youre a whale, conglatulations!"
+      
+bmiTell :: Double -> Double -> String
+bmiTell weight height
+  | weight / height ^ 2 <= 18.5 = "You're underweight, you emo, you!"
+  | weight / height ^ 2 <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"
+  | weight / height ^ 2 <= 30.0 = "You're fat! Lose some weight, fatty!"
+  | otherwise   = "Youre a whale, conglatulations!"
